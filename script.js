3418bc51ced334f559ae9b9a9423bd50dc01dbb5
LesleyBeijl <99062581@mydavinci.nl>
2021-03-09 10:48:43 +0100

    Buttons goed gemaakt

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 //alert("Welkom bij Jacks Hutje!");
 
-function hoi(){
-	document.write("Hallo wereld");
+//function hoi(){
+	//document.write("Hallo wereld");}
+//hoi();
+
+//var drankje = prompt("Welke bestelling wilt u toevoegen?");
+
+function drankje(){
+	var drankje = prompt("Welk drankje wilt u bestellen?");
 }
 
-hoi();
+function gerecht(){
+	var gerecht = prompt("Welk gerecht wilt u bestellen?");
+}
+function dessert(){
+	var dessert = prompt("Welk dessert wilt u bestellen?");
+}
