27779eb7cd82cbce819fbcef868eed83f5a07844
LesleyBeijl <99062581@mydavinci.nl>
2021-03-12 09:28:34 +0100

    button en functies gemaakt

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//alert("Welkom bij Jacks Hutje!");
-
+alert("Welkom bij Jacks Hutje!");
+document.write("Als je klaar bent met bestellen type 'stop' in om te stoppen met de bestelling en af te ronden met betalen of nog iets anders bestellen");
 //function hoi(){
 	//document.write("Hallo wereld");}
 //hoi();
@@ -7,12 +7,25 @@
 //var drankje = prompt("Welke bestelling wilt u toevoegen?");
 
 function drankje(){
+	for (var i = 1; i < 10; i++){
+	
 	var drankje = prompt("Welk drankje wilt u bestellen?");
-}
+								}
+					}
 
 function gerecht(){
+	for (var i = 1; i < 10; i++){
+
 	var gerecht = prompt("Welk gerecht wilt u bestellen?");
-}
+								}
+					}	
+
 function dessert(){
+	for (var i = 1; i < 10; i++){
 	var dessert = prompt("Welk dessert wilt u bestellen?");
+								}
+					}
+
+function klaar(){
+	var klaar = alert("Bedankt voor het bestellen bij Jacks Hutje!");
 }
